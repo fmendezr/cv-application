1b4682373f3697aedff1c2843f17f2efb7e492ef
Felix Mendez <mendez.felixrn@gmail.com>
2022-12-25 23:39:29 -0500

    create a mockup of footer component (lacks icon)

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy fmendez <i href="https://github.com/fmendezr"> github</i> </p>
+        </footer>
+    )
+}
+
+export default Footer;

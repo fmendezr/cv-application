ba9ea2c2531d6e59143848c230564d6e807fe8ea
Felix Mendez <mendez.felixrn@gmail.com>
2022-12-28 12:00:28 -0500

    makje functions that change state and pass them as props to Form component

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,15 +5,19 @@ import Form from "./Form";
 class Main extends Component {
     constructor(props){
         super(props);
+        this.handlefirstNameChange = this.handlefirstNameChange.bind(this);
+        this.handleSecondNameChange = this.handleSecondNameChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handleAddressChange = this.handleAddressChange.bind(this);
+        this.handlePhoneChange = this.handlePhoneChange.bind(this);
+        this.handleGoalChange = this.handleGoalChange.bind(this);
         this.state = {
-            firstname: "",
+            firstName: "",
             lastname: "",
-            title: "",
-            photo: "",
+            email: "", 
             address: "", 
-            phoneNumber: "",
-            email: "",
-            description: "",
+            phone: "",
+            goal: "",
             experiece: [],
             education: [],
             skills: [],
@@ -21,10 +25,41 @@ class Main extends Component {
         };
     }
 
+    handlefirstNameChange(firstName){
+        this.setState({firstName});
+    }
+
+    handleSecondNameChange(lastname){
+        this.setState({lastname});
+    }
+
+    handleEmailChange(email){
+        this.setState({email});
+    }
+
+    handleAddressChange(address){
+        this.setState({address});
+    }
+
+    handlePhoneChange(phone){
+        this.setState({phone});
+    }
+
+    handleGoalChange(goal){
+        this.setState({goal});
+    }
+
     render(){
         return (
             <main>
-                <Form />
+                <Form 
+                    onFirstNameChange={this.handlefirstNameChange}
+                    onSecondNameChange={this.handleSecondNameChange}
+                    onEmailChange={this.handleEmailChange}
+                    onAddressChange={this.handleAddressChange}
+                    onPhoneChange={this.handlePhoneChange}
+                    onGoalChange={this.handleGoalChange}
+                />
             </main>
         )
     }

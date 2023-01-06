cb627a74a32ab765c4330a0fcb252daa2c479114
Felix Mendez <mendez.felixrn@gmail.com>
2023-01-05 22:13:29 -0500

    basic styling to btns section

--- src/components/Form/ButtonSection.js
+++ src/components/Form/ButtonSection.js
@@ -6,11 +6,26 @@ class ButtonSection extends Component {
     }
     render (){
         return (
-            <section>
-                <button onClick={this.props.onReset}> Reset</button>
-                <button onClick={this.props.onSample}> Sample CV</button>
+            <section style={stylingObject.section}>
+                <button style={stylingObject.btn} onClick={this.props.onReset}> Reset</button>
+                <button style={stylingObject.btn} onClick={this.props.onSample}> Sample CV</button>
             </section>
         )
+    }
+}
+
+const stylingObject = { 
+    section: {
+        display: "flex",
+        justifyContent: "space-between"
+    },
+    btn: {
+        width: "45%",
+        height: "1.8rem",
+        fontSize: "1rem",
+        fontWeight: "bold",
+        border: "2px solid #000033",
+        backgroundColor: "rgba(0, 0, 44, 0.326)"
     }
 }
 

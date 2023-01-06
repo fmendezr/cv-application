e742bf0427ecebb1fde5a973a0fc0f2e058c01dd
Felix Mendez <mendez.felixrn@gmail.com>
2023-01-05 22:37:18 -0500

    add logo and styling to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,27 @@
 import React from "react";
+import logo from "../components/github.svg"
 
 const Footer = () => {
     return (
-        <footer>
-            <p>Copyright &copy fmendez <i href="https://github.com/fmendezr"> github</i> </p>
+        <footer style={stylingObject.footer}> 
+            <a href="https://github.com/fmendezr"><p style={stylingObject.p}>Copyright &#169; fmendez <img style={stylingObject.img} src={logo} /></p></a>
         </footer>
     )
+}
+
+const stylingObject = {
+    img: {
+        height: "1.3rem"
+    },
+    p: {
+        fontSize: "1.5rem",
+        color: "#000033",
+    },
+    footer: {
+        margin: "1rem 0rem",
+        display: "flex",
+        justifyContent: "center"
+    }  
 }
 
 export default Footer;
